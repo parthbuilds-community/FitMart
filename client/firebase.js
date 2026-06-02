c3fd154300bf024b6090e66d7d97a7026b221c3c
shilpayadavoffical <shilpayadavmail@gmail.com>
2026-06-03 00:40:00 +0530

    fix firebase configuartion

---
+++ client/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwYiylxlbuKQSLjaycXAUEVfx4a3CgTmE",
+  authDomain: "fitmart-a3af7.firebaseapp.com",
+  projectId: "fitmart-a3af7",
+  storageBucket: "fitmart-a3af7.firebasestorage.app",
+  messagingSenderId: "79630003510",
+  appId: "1:79630003510:web:4548c55769357767e01797"
+};
+
+export const app = initializeApp(firebaseConfig); 
