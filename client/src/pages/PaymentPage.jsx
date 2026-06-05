da5172946506142c9cbe313785597367f78dea9a
vikash-kun <12345vikash@gmail.com>
2026-06-05 12:00:41 +0530

    refactor: remove duplicate Google Fonts @import from all components

--- client/src/pages/PaymentPage.jsx
+++ client/src/pages/PaymentPage.jsx
@@ -302,7 +302,6 @@ export default function PaymentPage() {
       />
 
       <style>
-        {`@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600&family=DM+Serif+Display&display=swap');`}
       </style>
 
       <div className="max-w-xl mx-auto px-4 sm:px-5 py-10 sm:py-16">
