f9e22310cfd04c0a5eed508858d267a834464252
Shivam <smishraa30@gmail.com>
2026-06-21 00:07:36 +0530

    feat: add PropTypes validation to Navbar and CartDrawer

--- client/src/components/CartDrawer.jsx
+++ client/src/components/CartDrawer.jsx
@@ -1,4 +1,5 @@
 // src/components/CartDrawer.jsx
+import PropTypes from "prop-types";
 import { useEffect } from "react";
 import { fmt } from "../utils/formatters";
 import { Link } from "react-router-dom";
@@ -226,5 +227,23 @@ function CartDrawer({
     </>
   );
 }
+CartDrawer.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      qty: PropTypes.number.isRequired,
+      image: PropTypes.string,
+      brand: PropTypes.string,
+    })
+  ).isRequired,
+  cartCount: PropTypes.number.isRequired,
+  cartTotal: PropTypes.number.isRequired,
+  updateQty: PropTypes.func.isRequired,
+  removeFromCart: PropTypes.func.isRequired,
+};
 
 export default CartDrawer;
